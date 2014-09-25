818cc5d33ddf189860a094704c2d305f583919cd
DonofLizards <katie@Katie.local>
2014-09-25 02:14:07 +0100

    Tractatus web crawler, to be integrated w/ Tweepy

---
+++ wittcrawler.py
@@ -0,0 +1,25 @@
+# To do: write bot. Parse out sentences using reg ex and delete duplicates
+# Check whether sentence has been tweeted before: if it hasn't, tweet it!
+# Do all of this in a cron job
+
+import requests
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+def make_soup(url):
+	r = requests.get(url)
+	page = r.text # Get the text contents of the page 
+	soup = BeautifulSoup(page)
+	return soup 
+
+soup = make_soup("http://tractatus-online.appspot.com/Tractatus/jonathan/body.html")
+aphorisms = soup.findAll("dd")
+
+strippedList = []
+for aphorism in aphorisms:
+	aphorism = aphorism.get_text() # Strip out all of the HTML 
+	strippedList.append(aphorism)
+
+# print(strippedList)
+
+print(list(OrderedDict.fromkeys(strippedList)))
