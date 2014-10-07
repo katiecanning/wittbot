5b4ef6ec54bc7ba779244c259953f34456ed59b0
DonofLizards <katie@Katie.local>
2014-10-07 03:29:36 +0100

    Revert "Revert "CV page completely revamped, still in progress""
    This reverts commit f4ca89aacf7720225b93fb03f28acb3cb0171125.

--- wittcrawler.py
+++ wittcrawler.py
@@ -5,6 +5,7 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import OrderedDict
+import re
 
 def make_soup(url):
 	r = requests.get(url)
@@ -20,6 +21,6 @@ for aphorism in aphorisms:
 	aphorism = aphorism.get_text() # Strip out all of the HTML 
 	strippedList.append(aphorism)
 
-# print(strippedList)
+print(strippedList)
 
-print(list(OrderedDict.fromkeys(strippedList)))
+# print(list(OrderedDict.fromkeys(strippedList)))
